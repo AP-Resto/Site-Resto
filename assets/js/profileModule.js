7e53e677c7968329fcb5721bc74f647b63588a30
Ryan <mrgamerstitan@gmail.com>
2023-09-28 17:22:12 +0200

    :tada: Ajout de la page des menus sur l'index

---
+++ assets/js/profileModule.js
@@ -0,0 +1,6 @@
+let profileHead = document.querySelector("#profile .head");
+const profile = document.querySelector("#profile");
+
+profileHead.addEventListener("click", () => {
+    profile.classList.toggle("expand");
+});
