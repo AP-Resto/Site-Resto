afa121522c21bf206168eaf5563c47fd48fa3490
MAURY Mathis <mathismaury31@gmail.com>
2023-11-16 15:50:56 +0100

    Commentaire pour commande_accepter.php

--- api/commande_accepter.php
+++ api/commande_accepter.php
@@ -2,19 +2,10 @@
 include("../assets/functions/ConnexionBDD.php");
 $connexionBdd = new ConnexionBDD();
 
-// Pour firefox et le formattage
+// Pour firefox on précice que le formattage est en format json
 header("Content-Type: application/json");
 
-if(!isset($_GET["id_commande"])){
-    $json = [
-        "success" => false,
-        "erreur" => "Vous devez fournir un id de commande avec le parametre URL ?id_commande=XX"
-    ];
-
-    echo json_encode($json);
-    die();
-}
-
+// On vérifie si l'ID de commande existe dans la base 
 $ID_commande = $_GET["id_commande"];
 if(!($connexionBdd->prepareAndFetchOne("SELECT * FROM commande WHERE id_commande = :idCommande", [":idCommande" => $ID_commande]))){
     $json = [
@@ -26,6 +17,17 @@ if(!($connexionBdd->prepareAndFetchOne("SELECT * FROM commande WHERE id_commande
     die();
 }
 
+// Si l'ID de commande est manquant, le script renvoie une réponse JSON indiquant l'erreur, puis le script se termine.
+if(!isset($_GET["id_commande"])){
+    $json = [
+        "success" => false,
+        "erreur" => "Vous devez fournir un id de commande avec le parametre URL ?id_commande=XX"
+    ];
+
+    echo json_encode($json);
+    die();
+}
+// On update l'état de l'id commande on le met à 1 ce qui représente l'état "en préparation"
 $commandes = $connexionBdd->prepareAndFetchOne(
     "UPDATE commande SET id_etat = :idEtat WHERE id_commande = :idCommande;",
     [
@@ -34,6 +36,7 @@ $commandes = $connexionBdd->prepareAndFetchOne(
     ]
 );
 
+//Après la mise à jour réussie, le script renvoie une réponse JSON indiquant le succès de l'opération.
 $json = [
     "success" => true,
     "message" => "La commande $ID_commande est maintenant acceptee !"
