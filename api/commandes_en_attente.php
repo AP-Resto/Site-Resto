766e667bcc19f66bc50fcb1f39a1ccd6b83bba27
Ryan <mrgamerstitan@gmail.com>
2024-02-29 14:51:36 +0100

    (PHP) Ajout des lignes de commandes aux objets de commande pour l'api en_attente

--- api/commandes_en_attente.php
+++ api/commandes_en_attente.php
@@ -4,13 +4,46 @@ include "../assets/functions/ReponseJson.php";
 
 $connexionBdd = new ConnexionBDD();
 
+
+// Sélectionner les commandes avec leurs lignes associées
 $commandes = $connexionBdd->prepareAndFetchAll(
-    "SELECT * FROM commande WHERE commande.id_etat = 1;"
+    "SELECT commande.*, ligne.*
+     FROM commande
+     INNER JOIN ligne ON commande.id_commande = ligne.id_commande
+     WHERE commande.id_etat = 1;"
 );
+
+// Organiser les commandes par leur ID pour regrouper les lignes
+$commandesGrouped = [];
+foreach ($commandes as $commande) {
+    $commandeId = $commande['id_commande'];
+    if (!isset($commandesGrouped[$commandeId])) {
+        // Si la commande n'existe pas dans le tableau regroupé, l'ajouter
+        $commandesGrouped[$commandeId] = [
+            'id_commande' => $commande['id_commande'],
+            'id_user' => $commande['id_user'],
+            'id_etat' => $commande['id_etat'],
+            'date' => $commande['date'],
+            'total_commande' => $commande['total_commande'],
+            'type_conso' => $commande['type_conso'],
+            'lignes' => [] // Initialiser un tableau pour les lignes de commande
+        ];
+    }
+    // Ajouter la ligne à la commande correspondante dans le tableau regroupé
+    $commandesGrouped[$commandeId]['lignes'][] = [
+        'id_ligne' => $commande['id_ligne'],
+        'id_produit' => $commande['id_produit'],
+        'qte' => $commande['qte'],
+        'total_ligne_ht' => $commande['total_ligne_ht']
+    ];
+}
+
+// Convertir le tableau regroupé en un tableau simple pour la réponse JSON
+$commandesGrouped = array_values($commandesGrouped);
 
 header("Content-Type: application/json");
 ReponseJson::repondre([
     "success" => true,
-    "nbCommandes" => count($commandes),
-    "commandes" => $commandes
+    "nbCommandes" => count($commandesGrouped),
+    "commandes" => $commandesGrouped
 ]);
